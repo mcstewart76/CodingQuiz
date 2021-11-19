2434a45c005909517c2059584aada0c4104249ef
mcstewart76 <mcstewart76@gmail.com>
2021-11-19 01:26:56 -0500

    Added all the questions to the question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,42 @@ const questions = [
             {text: "booleans", right: false },
             {text: "alerts", right: true },
             {text: "numbers", right: false }
-
         ]
-    }
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____. ",
+        answers: [
+            {text: "quotes", right: false },
+            {text: "curly brackets", right: false },
+            {text: "parentheses", right: true },
+            {text: "square brackets", right: false }
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answers: [
+            {text: "numbers and strings", right: false },
+            {text: "other arrays", right: false },
+            {text: "booleans", right: false },
+            {text: "all of the above", right: true }
+        ]
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: [
+            {text: "commas", right: false },
+            {text: "curly brackets", right: false },
+            {text: "quotes", right: true },
+            {text: "parentheses", right: false }
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            {text: "Javascript", right: false },
+            {text: "terminal/bash", right: false },
+            {text: "for loops", right: false },
+            {text: "console.log", right: true }
+        ]
+    },
 ]
