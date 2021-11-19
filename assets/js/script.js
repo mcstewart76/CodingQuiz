2c5a0cecb384499945bc3c1fd9e1a89fccbbd207
mcstewart76 <mcstewart76@gmail.com>
2021-11-18 21:02:40 -0500

    Started an array with questions to be used

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            {text: "strings", right: false },
+            {text: "booleans", right: false },
+            {text: "alerts", right: true },
+            {text: "numbers", right: false }
+
+        ]
+    }
+]
